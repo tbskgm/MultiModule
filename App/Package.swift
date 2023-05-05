def3770d32a04df88b66f0bb9bc365167f4e7604
tbskgm <yxiaoju01@gmail.com>
2023-05-05 23:29:29 +0900

    add: マルチモジュール対応

---
+++ App/Package.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by 小駒翼 on 2023/05/05.
+//
+
+import PackageDescription
+
+let package = Package(
+  name: "",
+  products: [],
+  dependencies: [],
+  targets: []
+)
---
+++ Sources/AppFeature/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by 小駒翼 on 2023/05/05.
+//
+
+import SwiftUI
+
+public struct ContentView: View {
+    public init() {}
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding()
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
