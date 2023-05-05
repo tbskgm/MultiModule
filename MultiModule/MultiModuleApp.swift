ea1a1760ff7f66406459d0773545091b7fee6ac1
tbskgm <yxiaoju01@gmail.com>
2023-05-05 17:52:02 +0900

    Initial Commit

---
+++ MultiModule/MultiModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiModuleApp.swift
+//  MultiModule
+//
+//  Created by 小駒翼 on 2023/05/05.
+//
+
+import SwiftUI
+
+@main
+struct MultiModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
